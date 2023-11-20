ce7e8a6b8e0fc02b16e6118ce31af0689d28bfed
NardosYosef <nardosyosef123@gmail.com>
2023-11-20 18:17:21 +0300

    esp_node

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const http = require('http');
+const https = require('https');
 const WebSocket = require('ws');
 
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 const port = 3000;
