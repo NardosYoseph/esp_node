d776c406c63ee8e4914dd9405c2829c2926de1cc
NardosYosef <nardosyosef123@gmail.com>
2023-11-21 11:31:49 +0300

    esp_node

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = https.createServer(app);
 const wss = new WebSocket.Server({ server ,secure:true});
 
-const port = 3000;
+const port = process.env.PORT||3000;
 app.use(express.static(__dirname));
 // Route to serve the HTML page with video player
 app.get('/video', (req, res) => {
