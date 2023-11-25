fb1eb476156b184cde793a23fbbfba21e230ff2c
NardosYosef <nardosyosef123@gmail.com>
2023-11-25 22:18:22 +0300

    node

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const http = require('http');
 const WebSocket = require('ws');
 const path = require('path');
 const fs = require('fs');
-// const privateKey = fs.readFileSync('key_no_passphrase.pem', 'utf8');
-// const certificate = fs.readFileSync('cert.pem', 'utf8');
-//const credentials = { key: privateKey, cert: certificate };
 const app = express();
 
 const server = http.createServer( app);
@@ -15,16 +12,16 @@ const wss = new WebSocket.Server({ server});
 
 const port = process.env.PORT||443;
 app.use(express.static(__dirname));
-
-app.get('/video', (req, res) => {
+app.ws('/video', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+
 wss.on('connection', (ws) => {
-  console.log('Client connected');
+
+  console.log('WebSocket connection established');
 
   ws.on('message', (frameData) => {
-
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(frameData, { binary: true });
