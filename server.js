904ea883699f665cd24459005b21166422f41def
NardosYosef <nardosyosef123@gmail.com>
2023-12-03 13:53:51 +0300

    nodeWs

--- server.js
+++ server.js
@@ -8,31 +8,47 @@ const app = express();
 
 const server = http.createServer( app);
 
-const wss = new WebSocket.Server({ server});
+const wss = new WebSocket.Server({ noServer: true});
 
-const port = process.env.PORT||443;
+const port = 3000;
 expressWs(app);
 app.use(express.static(__dirname));
 
-// app.ws('/video', (ws, res) => {
-//   res.sendFile(path.join(__dirname + '/index.html'));
-//     ws.send(data);
-// });
+app.ws('/video', (ws, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+  ws.on('message', (frameData) => {
+    ws.send(frameData, { binary: true });
+  });
+});
 
 
 wss.on('connection', (ws) => {
 
   console.log('WebSocket connection established');
-
+ 
   ws.on('message', (frameData) => {
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(frameData, { binary: true });
-      }
-    });
+    if (frameData instanceof Buffer) {
+      wss.clients.forEach((client) => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+         
+          client.send(frameData, { binary: true }, (error) => {
+            if (error) {
+              console.error(`Error sending binary data to client: ${error.message}`);
+            }
+          });
+        }
+      });
+    } else {
+      console.error('Received non-binary data. Ignoring.');
+    }
+  });
+ 
+});
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request);
   });
 });
-
 
 server.listen(port, () => {
   console.log(`Server is running on port ${server.address().port}`);
