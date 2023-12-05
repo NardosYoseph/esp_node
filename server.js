c48008a2f3cc50fdff6633aa009ee7a08ce837b5
NardosYosef <nardosyosef123@gmail.com>
2023-12-05 11:00:06 +0300

    nodeWs

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer( app);
 
 const wss = new WebSocket.Server({ server});
 
-const port =443;
+const port =process.env.PORT||443;
 expressWs(app);
 app.use(express.static(__dirname));
 
